903b4186fb7f65ec4c27015865d03b3a6aa0f3a3
Celeste Moore <celestealexmoore@gmail.com>
2021-04-13 01:01:34 -0500

    Updating for AskBCS question...

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,41 +7,42 @@ var apiKey = `cc39f5ebdea0803927099971a33add46`
 
 var API_KEY = 'cc39f5ebdea0803927099971a33add46';
 var APP_ID = 'daae60e1';
-var searchTerm = 'cabbage';
+var searchTerm = 'Dairy Free';
 
 var endpoint = `https://api.edamam.com/search?q=${searchTerm}&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=6&calories=591-722`;
 
 fetch(endpoint)
-	.then(function (res) { 
-		return res.json();
-	 })
-	 .then(function (data){
-		 console.log(data);
-
-		 //data we want to capture
+	.then((res) => (res.json())
+	.then((data) => {
+		console.log(data);
+		//data we want to capture
 		console.log(data.hits[2]);
 
-        
+        var cardAppend = document.getElementsByClassName(".card");
 
-        var cardAppend = document.getElementsByClassName(".filled-in");
-        for (var i = 0; i< cardAppend.length; i++){
+        for (var i = 0; i < cardAppend.length; i++){
 
-            var cardTitle = document.getElementsByClassName(".card-title");
-            cardTitle.textContent = ();
+            var cardAppend = document.getElementsByClassName(".card");
+
+            var cardTitle = document.createElement('h4');
+            cardTitle.classList.add('card-title');
+            cardTitle.textContent = (`${data.hits[2].recipe.label}`);
 
 
             var cardContent = document.getElementsByClassName(".card-content");
-            cardContent.textContent = ();
+            // cardContent.textContent = ();
 
             var cardImage = document.getElementsByClassName(".card-image");
-            cardImage.setAttribute(
-                'src',
-                ``
-            );
+            // cardImage.setAttribute(
+            //     'src',
+            //     ``
+            // );
 
             var cardLink = document.getElementsByClassName(".card-link");
-            cardLink.textContent = ();
+            // cardLink.textContent = ();
 
-        }
-		
-	 })
+            cardAppend[i].appendChild(cardTitle);
+        };
+    })
+    
+);
