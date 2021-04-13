10431816ca94b8322a35b7c447924fff82497e3a
Celeste Moore <celestealexmoore@gmail.com>
2021-04-12 22:40:15 -0500

    Additions primarily to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var API_KEY = 'cc39f5ebdea0803927099971a33add46';
 var APP_ID = 'daae60e1';
 var searchTerm = 'cabbage';
 
-var endpoint = `https://api.edamam.com/search?q=${searchTerm}&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=5&calories=591-722`;
+var endpoint = `https://api.edamam.com/search?q=${searchTerm}&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=6&calories=591-722`;
 
 fetch(endpoint)
 	.then(function (res) { 
@@ -20,5 +20,28 @@ fetch(endpoint)
 
 		 //data we want to capture
 		console.log(data.hits[2]);
+
+        
+
+        var cardAppend = document.getElementsByClassName(".filled-in");
+        for (var i = 0; i< cardAppend.length; i++){
+
+            var cardTitle = document.getElementsByClassName(".card-title");
+            cardTitle.textContent = ();
+
+
+            var cardContent = document.getElementsByClassName(".card-content");
+            cardContent.textContent = ();
+
+            var cardImage = document.getElementsByClassName(".card-image");
+            cardImage.setAttribute(
+                'src',
+                ``
+            );
+
+            var cardLink = document.getElementsByClassName(".card-link");
+            cardLink.textContent = ();
+
+        }
 		
 	 })
