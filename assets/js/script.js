d47c904aa95b6f439dd4e432673ad2cbc108c030
MohammedOttun <ottunmo@mac.com>
2021-04-17 09:41:16 -0500

    Added logic to dynamically generate cards from API search results.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,7 @@
-//Design tool for recipe cards.
-var contentCard = "https://material-ui.com/components/cards/#RecipeReviewCard.js";
-var apiID = `daae60e1`;
-var apiKey = `cc39f5ebdea0803927099971a33add46`;
-
 // Edamam API Calls for Recipe
-
 var API_KEY = 'cc39f5ebdea0803927099971a33add46';
 var APP_ID = 'daae60e1';
-var searchTerm = 'Dairy Free';
-
-// Edamam API Calls for Recipe
-
-var API_KEY = 'cc39f5ebdea0803927099971a33add46';
-var APP_ID = 'daae60e1';
+var searchTerm;
 
 // For sending data for favorite recipes to local storage
 
@@ -42,15 +31,15 @@ var clearResults = document.getElementById("clearResults");
 
 //Coding Starts Here:
 
-function clearCards(){
+function clearCards() {
     //hide bottom section
-    var bottomSection = document.getElementById("bottom-section")
-    bottomSection.innerHTML="";
+    var bottomSection = document.getElementById("recipe-returns")
+    bottomSection.innerHTML = "";
 };
 
 clearResults.addEventListener("click", function (e) {
-    hideDiv(inputSearchTerm) //when checked
-}, {once: true}); //disable once clicked once
+    clearCards();   //when checked
+}, { once: true }); //disable once clicked once
 
 
 // function clearDiv(cardAppend){
@@ -61,7 +50,7 @@ clearResults.addEventListener("click", function (e) {
 
 
 
-function showDiv(inputSearchTerm){
+function showDiv(inputSearchTerm) {
     //hide bottom section
     var bottomSection = document.getElementById("bottom-section")
     bottomSection.setAttribute("class", "hide");
@@ -76,99 +65,136 @@ function showDiv(inputSearchTerm){
 glutenAllergy.addEventListener("click", function (e) {
     console.log(true)
     showDiv(searchTermGluten); //when checked
-}, {once: true}); //disable once clicked once
+}, { once: true }); //disable once clicked once
 
 peanutAllergy.addEventListener("click", function (e) {
     console.log(e.target)
     showDiv(searchTermPeanut); //when checked
-}, {once: true}); //disable once clicked once
+}, { once: true }); //disable once clicked once
 
 vegan.addEventListener("click", function (e) {
     console.log(e.target)
     showDiv(searchTermVegan); //when checked
-}, {once: true}); //disable once clicked once
+}, { once: true }); //disable once clicked once
 
 vegetarian.addEventListener("click", function (e) {
     console.log(e.target)
     showDiv(searchTermVegetarian); //when checked
-}, {once: true}); //disable once clicked once
+}, { once: true }); //disable once clicked once
 
 porkFree.addEventListener("click", function (e) {
     console.log(e.target)
     showDiv(searchTermPorkFree); //when checked
-}, {once: true}); //disable once clicked once
+}, { once: true }); //disable once clicked once
 
 kosher.addEventListener("click", function (e) {
     console.log(e.target)
     showDiv(searchTermKosher); //when checked
-}, {once: true}); //disable once clicked once
+}, { once: true }); //disable once clicked once
 
 soy.addEventListener("click", function (e) {
     console.log(e.target)
     showDiv(searchTermSoy); //when checked
-}, {once: true}); //disable once clicked once
+}, { once: true }); //disable once clicked once
 
 
-//API response:
-function searchAPI(searchTerm) {
-
-    var endpoint = `https://api.edamam.com/search?q=${searchTerm}&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=10&calories=591-722`;
-
-    fetch(endpoint)
-        .then((res) => (res.json())
-        .then((data) => {
-            console.log(data);
-
-            var cardAppend = document.getElementsByClassName("card");
-
-            for (var i = 0; i < data.hits.length; i++){
-
-                var cardAppend = document.getElementsByClassName("card");
-                // cardAppend.innerHTML = "";
-
-                var cardTitle = document.createElement('h6');
-                cardTitle.classList.add('card-title');
-                cardTitle.textContent = data.hits[i].recipe.label;
-
-                var cardLink = document.createElement('a');
-                cardLink.classList.add('card-url');
-                cardLink.setAttribute(
-                    'href',
-                    `${data.hits[i].recipe.url}`
-                );
-                cardLink.textContent = "Click to view recipe!"
-                console.log(cardLink);
-
-                var cardImage = document.createElement('img');
-                cardImage.classList.add('card-image');
-                cardImage.setAttribute(
-                    'src',
-                     `${data.hits[i].recipe.image}`
-                );
-
-                cardAppend[i].appendChild(cardTitle);
-                cardAppend[i].appendChild(cardImage);
-                cardAppend[i].appendChild(cardLink);
-                console.log(cardAppend[i])
-            };
-        })
-        
-    );
+//Event Listner for click to toggle
+var playlist_buttons = document.getElementsByClassName('fas fa-plus');
+for (var i = 0; i < playlist_buttons.length; i++) {
+    playlist_buttons[i].addEventListener('click', togglePlaylistButton);
 }
+//Toggle Function
+function change(iconID) {
+    if (document.getElementById(iconID).className == "fa fas fa-plus") {
+        document.getElementById(iconID).className = "fas fa-check";
+    } else {
+        document.getElementById(iconID).className = "fa fas fa-plus";
+    }
+}
+
+    //API response:
+    function searchAPI(searchTerm) {
+
+        var RECIPE_SEARCH_ENDPOINT = `https://api.edamam.com/search?q=${searchTerm}&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=10&calories=591-722`;
+
+        fetch(RECIPE_SEARCH_ENDPOINT)
+            .then((res) => (res.json())
+                .then((data) => {
+                    console.log(data);
+
+                    var cardAppend = document.getElementsByClassName("card");
+
+                    for (var i = 0; i < data.hits.length; i++) {
+
+                        // create card elements
+                        var recipesEL = document.getElementById('recipe-returns');
+
+                        var cardEl = document.createElement('div');
+                        cardEl.classList.add('card');
+                        cardEl.setAttribute('href', `${data.hits[i].recipe.url}`);
+                        recipesEL.appendChild(cardEl);
+
+                        var cardContentEl = document.createElement('div');
+                        cardContentEl.classList.add('card-content');
+
+                        var cardTitleEl = document.createElement('h6');
+                        cardTitleEl.classList.add('card-title');
+                        cardTitleEl.textContent = data.hits[i].recipe.label;
+
+                        var cardImageEl = document.createElement('img');
+                        cardImageEl.classList.add('card-image');
+                        cardImageEl.setAttribute('src', `${data.hits[i].recipe.image}`);
+
+                        var iconWrapperEl = document.createElement('div');
+                        iconWrapperEl.classList.add('iconwrapper');
+                        var iconEl = document.createElement('i');
+                        iconEl.classList.add('fas');
+                        iconEl.classList.add('fa-plus');
+                        iconEl.setAttribute('id', "icon1");
+                        iconEl.setAttribute('onClick', "change('icon1')");
+                        iconEl.setAttribute('aria-disabled', 'true');
+                        iconEl.setAttribute('hidden', "true");
+                        iconWrapperEl.appendChild(iconEl);
+
+
+                        var cardLinkEL = document.createElement('a');
+                        cardLinkEL.classList.add('card-url');
+                        cardLinkEL.setAttribute('href', `${data.hits[i].recipe.url}`);
+                        cardLinkEL.textContent = "Click to view recipe!"
+
+                        // build card elements
+                        cardEl.appendChild(cardContentEl);
+                        cardContentEl.appendChild(cardTitleEl);
+                        cardEl.appendChild(cardImageEl);
+                        cardEl.appendChild(iconWrapperEl);
+                        cardEl.appendChild(cardLinkEL);
+
+                    };
+
+                    // var rceipeObject = {
+                    //     cardTtitle: data.hits[i].recipe.label,
+                    //     cardImage: data.hits[i].recipe.image,
+                    //     cardLink: data.hits[i].recipe.url
+                    // }
+                    // console.log("Recipe Object is " + rceipeObject);
+
+                })
+
+            );
+    }
 
 
 // For adding data to Local Storage
 // favoriteRecipe.addEventListener("click", function(event){
 //     event.preventDefault();
- 
+
 //     var submission ={
 //         favoriteRecipe: favoriteRecipe.value,
 //     };
- 
+
 //     console.log(JSON.stringify(submission));
-    
+
 //     localStorage.setItem("submission", JSON.stringify(submission));
- 
+
 //     var submissionParse = JSON.parse(localStorage.getItem("submission"));
 // });
-
